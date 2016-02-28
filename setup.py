e128f657396d3f677b6f3c53b1f5609019f12f0a
Italo Rossi <italorossib@gmail.com>
2016-02-28 20:49:05 -0300

    Making it work in pypi

--- setup.py
+++ setup.py
@@ -8,21 +8,18 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 with open('requirements.txt') as f:
     requires = f.readlines()
 
 setup(
     name='greenswitch',
-    version='0.0.2',
+    version='0.0.3',
     description=u'Battle proven FreeSWITCH Event Socket Protocol client implementation with Gevent.',
     long_description=readme,
     author=u'Ítalo Rossi',
     author_email=u'italorossib@gmail.com',
     url=u'https://github.com/italorossi/greenswitch',
-    license=license,
+    license=u'MIT',
     packages=find_packages(exclude=('tests', 'docs')),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
