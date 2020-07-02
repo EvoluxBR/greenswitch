17dbc47c48799c6bfaad0bdc6178bb09558d3f1a
Evangilo Morais <evangilo@evolux.net.br>
2020-07-02 18:11:09 -0300

    create while_connected class #59

---
+++ tests/test_helpers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import mock
+import pytest
+
+from greenswitch import esl, helpers
+
+
+@pytest.mark.usefixtures("outbound_session")
+class TestWhileConnected(unittest.TestCase):
+    def setUp(self):
+        self.outbound_session.connect()
+        self.execute_slow_task = mock.MagicMock()
+
+    def test_context_manager(self):
+        with self.assertRaises(esl.OutboundSessionHasGoneAway):
+            with helpers.while_connected(self.outbound_session):
+                self.outbound_session.on_hangup(None)
+                self.execute_slow_task()
+
+        self.execute_slow_task.assert_called()
+
+    def test_decorator(self):
+        @helpers.while_connected(self.outbound_session)
+        def myflow():
+            self.outbound_session.on_hangup(None)
+            self.execute_slow_task()
+
+        with self.assertRaises(esl.OutboundSessionHasGoneAway):
+            myflow()
+
+        self.execute_slow_task.assert_called()
+
+    def test_skip_code_execution_if_the_outbound_session_is_disconnected(self):
+        with self.assertRaises(esl.OutboundSessionHasGoneAway):
+            self.outbound_session.on_hangup(None)
+
+            with helpers.while_connected(self.outbound_session):
+                self.execute_slow_task()
+
+        self.execute_slow_task.assert_not_called()
+
+    def test_raise_value_error_while_the_call_is_active(self):
+        with self.assertRaises(ValueError):
+            with helpers.while_connected(self.outbound_session):
+                raise ValueError("http exception")
