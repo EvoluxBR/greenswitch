8c553bfc1295b0d7c8a434b4cb25d97be33319fb
Ítalo Rossi <italorossib@gmail.com>
2020-07-07 16:02:24 -0300

    version bump - v0.0.11

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='greenswitch',
-    version='0.0.10',
+    version='0.0.11',
     description=u'Battle proven FreeSWITCH Event Socket Protocol client implementation with Gevent.',
     long_description=readme,
     author=u'Ítalo Rossi',
